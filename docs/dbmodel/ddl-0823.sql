dbb969db3ab19177e55649cffe1c262c17f6e15e
smallonestep <hankwan777@gmail.com>
2018-08-23 10:56:42 +0900

    ㅁㅁ

--- docs/dbmodel/ddl-0823.sql
+++ docs/dbmodel/ddl-0823.sql
@@ -110,9 +110,9 @@ ALTER TABLE P0_RSV
 CREATE TABLE P0_GRD (
     TNO INTEGER NOT NULL COMMENT '운항번호', -- 운항번호
     CLS INTEGER NOT NULL COMMENT '좌석등급', -- 좌석등급
-    PNO INTEGER NULL     COMMENT '가격번호', -- 가격번호
-    PRC INTEGER NULL     COMMENT '가격', -- 가격
-    SNO INTEGER NULL     COMMENT '총좌석수' -- 총좌석수
+    PNO INTEGER NOT NULL COMMENT '가격번호', -- 가격번호
+    PRC INTEGER NOT NULL COMMENT '가격', -- 가격
+    SNO INTEGER NOT NULL COMMENT '총좌석수' -- 총좌석수
 )
 COMMENT '좌석정보';
 
@@ -123,6 +123,12 @@ ALTER TABLE P0_GRD
             TNO, -- 운항번호
             CLS  -- 좌석등급
         );
+
+-- 좌석정보 유니크 인덱스
+CREATE UNIQUE INDEX UIX_P0_GRD
+    ON P0_GRD ( -- 좌석정보
+        PNO ASC -- 가격번호
+    );
 
 -- 운항정보
 ALTER TABLE P0_TRVL
